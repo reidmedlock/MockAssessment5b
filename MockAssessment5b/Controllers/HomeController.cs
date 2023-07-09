5270d0e6cd77d289c009d1e2950fb9d5326205e6
reidmedlock <benmedlock19@gmail.com>
2023-07-09 17:18:06 -0400

    Add project files.

---
+++ MockAssessment5b/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MockAssessment5b.Models;
+
+namespace MockAssessment5b.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult GetAge()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Result(Person person)
+        {
+            if (person.Age >= 21)
+                person.CanDrink = true;
+            else
+                person.CanDrink = false;
+
+            if (person.Age >= 16)
+                person.CanDrive = true;
+            else
+                person.CanDrive = false;
+
+            return View(person);
+        }
+    }
+}
---
+++ MockAssessment5b/Models/PersonModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MockAssessment5b.Models
+{
+	public class Person
+	{
+		public int Age { get; set; }
+		public string UserName { get; set; }
+		public bool CanDrink { get; set; }
+		public bool CanDrive { get; set; }
+	}
+}
